f647ae6d7a215be547be032b0edc7261728488a1
Hugo <jhugodc@gmail.com>
2023-02-27 10:56:01 -0700

    default config

---
+++ nimbus/start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+if [ $1 = "-v" ]; then
+    evm_nimbus --version
+else
+    stateProvided=0
+    readErrorLog=0
+    errorLogFile=""
+    cmdArgs=""
+    for index in ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} ${23} ${24} ${25} ${26}; do
+        if [ $index = "--input.alloc" ]; then
+            stateProvided=1
+        fi
+        if [ $readErrorLog -eq 1 ]; then
+            errorLogFile=$index
+            readErrorLog=0
+            continue
+        fi
+        if [ $index = "--output.errorlog" ]; then
+            readErrorLog=1
+            continue
+        fi
+        cmdArgs=$cmdArgs" "$index
+    done
+    if [ $stateProvided -eq 1 ]; then
+        evm_nimbus $cmdArgs --verbosity 2 2> $errorLogFile
+    else
+        evm_nimbus $cmdArgs 2> $errorLogFile
+    fi
+fi
