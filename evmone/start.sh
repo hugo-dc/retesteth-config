6691058d5d49d7fe7f43e27330adca8aed9974b1
Hugo <jhugodc@gmail.com>
2023-04-17 18:51:02 -0700

    Fix evmone start

--- evmone/start.sh
+++ evmone/start.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-wevm=$(which evmone)
+wevm=$(which evmone-t8n)
+
 if [ -z $wevm ]; then
    >&2 echo "Can't find EvmOne's 'evmone' executable alias in the system path!"
    exit 1
 fi
 
 if [ $1 = "t8n" ] || [ $1 = "b11r" ]; then
-    evmone $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17 $18 $19 $20 $21 $22 $23 $24 $25 $26
+    evmone-t8n $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 $13 $14 $15 $16 $17 $18 $19 $20 $21 $22 $23 $24 $25 $26
 elif [ $1 = "-v" ]; then
-    evmone -v
+    evmone-t8n -v
+elif [ $1 = "eof" ]; then
+  #echo "2: $2 3: $3 4: $4 5: $5"
+  echo $5 | evmone-eofparse --fork $3 
 else
     stateProvided=0
     readErrorLog=0
@@ -30,9 +34,12 @@ else
         fi
         cmdArgs=$cmdArgs" "$index
     done
-    if [ $stateProvided -eq 1 ]; then
-        evmone $cmdArgs --verbosity 2 2> $errorLogFile
-    else
-        evmone t9n $cmdArgs 2> $errorLogFile
+    if [ ! -z "$errorLogFile" ]; then
+      if [ $stateProvided -eq 1 ]; then
+          echo $1
+          evmone-t8n $cmdArgs --verbosity 2 2> $errorLogFile
+      else
+          evmone-t9n $cmdArgs 2> $errorLogFile
+      fi
     fi
 fi
